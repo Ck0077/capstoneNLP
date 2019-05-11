5a41df8541f2b6aabe12ae562a11114cd31e40ca
Ck0077 <43908223+Ck0077@users.noreply.github.com>
2019-05-11 14:40:42 +0530

    Corrected files

--- server.R
+++ server.R
@@ -7,9 +7,17 @@
 #    http://shiny.rstudio.com/
 #
 
-suppressWarnings(library(tm))
-suppressWarnings(library(stringr))
-suppressWarnings(library(shiny))
+
+suppressPackageStartupMessages({
+  library(tidytext)
+  library(tm)
+  library(tidyr)
+  library(dplyr)
+  library(ggplot2)
+  library(stringr)
+  library(knitr)
+  library(shiny)
+})
 
 # Load Quadgram,Trigram & Bigram & Unigram Data frame files
 
@@ -18,6 +26,72 @@ trigram <- readRDS("trigram.RData");
 bigram <- readRDS("bigram.RData");
 unigram <- readRDS("unigram.RData");
 mesg <<- ""
+
+
+bi_words <- readRDS("bi_words_fast.rds")
+tri_words  <- readRDS("tri_words_fast.rds")
+quad_words <- readRDS("quad_words_fast.rds")
+
+bigram <- function(input_words){
+  num <- length(input_words)
+  filter(bi_words, 
+         word1==input_words[num]) %>% 
+    top_n(1, n) %>%
+    filter(row_number() == 1L) %>%
+    select(num_range("word", 2)) %>%
+    as.character() -> out
+  ifelse(out =="character(0)", "?", return(out))
+}
+
+
+trigram <- function(input_words){
+  num <- length(input_words)
+  filter(tri_words, 
+         word1==input_words[num-1], 
+         word2==input_words[num])  %>% 
+    top_n(1, n) %>%
+    filter(row_number() == 1L) %>%
+    select(num_range("word", 3)) %>%
+    as.character() -> out
+  ifelse(out=="character(0)", bigram(input_words), return(out))
+}
+
+
+quadgram <- function(input_words){
+  num <- length(input_words)
+  filter(quad_words, 
+         word1==input_words[num-2], 
+         word2==input_words[num-1], 
+         word3==input_words[num])  %>% 
+    top_n(1, n) %>%
+    filter(row_number() == 1L) %>%
+    select(num_range("word", 4)) %>%
+    as.character() -> out
+  ifelse(out=="character(0)", trigram(input_words), return(out))
+}
+
+
+ngrams <- function(input){
+  # Create a dataframe
+  input <- data_frame(text = input)
+  # Clean the Inpput
+  replace_reg <- "[^[:alpha:][:space:]]*"
+  input <- input %>%
+    mutate(text = str_replace_all(text, replace_reg, ""))
+  # Find word count, separate words, lower case
+  input_count <- str_count(input, boundary("word"))
+  input_words <- unlist(str_split(input, boundary("word")))
+  input_words <- tolower(input_words)
+  # Call the matching functions
+  out <- ifelse(input_count == 0, "Please input a phrase",
+                ifelse(input_count == 3, quadgram(input_words),
+                       ifelse(input_count == 2, trigram(input_words), bigram(input_words))))
+  
+  # Output
+  return(out)
+}
+
+
 
 # Cleaning of user input before predicting the next word
 
@@ -62,7 +136,8 @@ Predict <- function(x) {
 shinyServer(function(input, output) {
    
   output$prediction <- renderPrint({
-    result <- Predict(input$inputString)
+    result <- ngrams(input$inputString)
+    #result <- Predict(input$inputString)
     output$text2 <- renderText({mesg})
     result
   });
